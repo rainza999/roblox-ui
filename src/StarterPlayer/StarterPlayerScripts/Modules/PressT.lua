03ec1daebdf2032aa6f3fa66912a9296ac7355e3
Rain <rain.patchanok@gmail.com>
2026-03-09 23:25:45 +0700

    x

--- src/StarterPlayer/StarterPlayerScripts/Modules/PressT.lua
+++ src/StarterPlayer/StarterPlayerScripts/Modules/PressT.lua
@@ -1,8 +1,28 @@
 local PressT = {}
 
 function PressT.tap()
+	local VirtualInputManager = game:GetService("VirtualInputManager")
+
 	print("Pressed T")
-	-- ใส่ logic ของคุณในโปรเจกต์ตัวเองตรงนี้
+
+	-- กด T
+	VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.T, false, game)
+	task.wait(0.1)
+	VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.T, false, game)
+end
+
+function PressT.startAntiAFK()
+	task.spawn(function()
+		while getgenv().RobloxUIRunning do
+			task.wait(600) -- 10 นาที (600 วินาที)
+
+			print("Anti AFK Trigger")
+
+			PressT.tap()
+			task.wait(0.5)
+			PressT.tap()
+		end
+	end)
 end
 
 return PressT
