9be68b5500c9767f312f83b89b9879bd8f247646
Rain <rain.patchanok@gmail.com>
2026-03-13 11:04:32 +0700

    x

--- src/StarterPlayer/StarterPlayerScripts/Modules/State.lua
+++ src/StarterPlayer/StarterPlayerScripts/Modules/State.lua
@@ -31,7 +31,7 @@ local State = {
 	},
 
 	selectedMinerals = {
-		["Blossom Boulder"] = true,
+		["Blossom Boulder"] = false,
 		["Glowy Rock"] = false,
 		["Floating Crystal"] = false,
 		["Heart Of The Island"] = false,
